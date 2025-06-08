5c2bbf489af4a267c7e865f83fd8909ff3312480
anvitgupta01 <anvitiitr@gmail.com>
2025-06-08 17:21:52 +0530

    Testing Self class object and name resolution

---
+++ test.py
@@ -0,0 +1,21 @@
+class self:
+    def func1(self):
+        print("anvit")
+
+class anvit:
+    class person:
+        def func1(self):
+            print("gupta")
+        def tester(self):
+            self().func1()
+
+anvit().person().tester()
+
+import file1 as f1
+
+f1.t(90)
+
+def f1():
+    print("anvit")
+
+f1.t(90)
